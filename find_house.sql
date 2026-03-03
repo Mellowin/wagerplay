c4260082aac9df01cfec073a47c7d972158dc275
Mellow <mellowin1987@gmail.com>
2026-03-03 16:29:25 +0200

    Remove all test-related files and changes

--- find_house.sql
+++
@@ -1,3 +0,0 @@
-SELECT id, email, "displayName" FROM users WHERE id = 'dd07667f-1f8c-4d0e-9850-b387d2044d47';
-UPDATE wallets SET "balanceWp" = 10000000 WHERE "userId" = 'dd07667f-1f8c-4d0e-9850-b387d2044d47';
-SELECT "displayName", "balanceWp" FROM users u LEFT JOIN wallets w ON u.id = w."userId" WHERE u.id = 'dd07667f-1f8c-4d0e-9850-b387d2044d47';
--- restore_house.sql
+++
@@ -1,2 +0,0 @@
-UPDATE wallets SET "balanceWp" = 10000000 WHERE "userId" = (SELECT id FROM users WHERE email = 'house@wagerplay.internal');
-SELECT "displayName", "balanceWp" FROM users u LEFT JOIN wallets w ON u.id = w."userId" WHERE email = 'house@wagerplay.internal';
